1f257b173cb603a58a9ab4b50aa0608f5f0b7b6c
Abstrekt <saammmmmmy@gmail.com>
2020-04-09 00:09:23 +0530

    formatting

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,187 +1,186 @@
-import {elements} from './base';
+import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
 const renderImage = image => {
-    const markup = `
+  const markup = `
         <div class="image" id="${image.data[0].nasa_id}" style=" background: url(https://images-assets.nasa.gov/image/${image.data[0].nasa_id}/${image.data[0].nasa_id}~thumb.jpg) rgba(71, 63, 78, .54)"></div> 
     `;
-    elements.searchResItems.insertAdjacentHTML('beforeend', markup);
-}
+  elements.searchResItems.insertAdjacentHTML("beforeend", markup);
+};
 
-export const renderResults  = images => {
-    images.forEach(renderImage);
-}
+export const renderResults = images => {
+  images.forEach(renderImage);
+};
 
 export const resHeader = (totalhits, query, resLength, pageNo) => {
-    let start = 1 + ((pageNo - 1) * 100);
-    let stop = resLength + ((pageNo - 1) * 100);
-    const lastPage = Math.floor(totalhits / 100);
-    if (lastPage == 0) {
-        elements.prevPage.style.display = 'none';
-        elements.nextPage.style.display = 'none';
-    }else if(pageNo == 1) {
-        elements.prevPage.style.display = 'none';
-        elements.nextPage.style.display = 'block';
-        elements.nextPage.innerHTML = '<i class="fas fa-caret-right"></i>';
-    }else if(pageNo == lastPage + 1) {
-        elements.prevPage.style.display = 'block';
-        elements.nextPage.style.display = 'none';
-        elements.prevPage.innerHTML = '<i class="fas fa-caret-left"></i>';
-    }else {
-        elements.prevPage.style.display = 'block';
-        elements.nextPage.style.display = 'block';
-        elements.nextPage.innerHTML = '<i class="fas fa-caret-right"></i>';
-        elements.prevPage.innerHTML = '<i class="fas fa-caret-left"></i>';
-    }
-    elements.totalhits.innerHTML = `Total hits: ${totalhits}`;
-    elements.resultsShowing.innerHTML =`Showing ${start}-${stop} out of ${totalhits} for query: "${query}"`;
-}
+  let start = 1 + (pageNo - 1) * 100;
+  let stop = resLength + (pageNo - 1) * 100;
+  const lastPage = Math.floor(totalhits / 100);
+  if (lastPage == 0) {
+    elements.prevPage.style.display = "none";
+    elements.nextPage.style.display = "none";
+  } else if (pageNo == 1) {
+    elements.prevPage.style.display = "none";
+    elements.nextPage.style.display = "block";
+    elements.nextPage.innerHTML = '<i class="fas fa-caret-right"></i>';
+  } else if (pageNo == lastPage + 1) {
+    elements.prevPage.style.display = "block";
+    elements.nextPage.style.display = "none";
+    elements.prevPage.innerHTML = '<i class="fas fa-caret-left"></i>';
+  } else {
+    elements.prevPage.style.display = "block";
+    elements.nextPage.style.display = "block";
+    elements.nextPage.innerHTML = '<i class="fas fa-caret-right"></i>';
+    elements.prevPage.innerHTML = '<i class="fas fa-caret-left"></i>';
+  }
+  elements.totalhits.innerHTML = `Total hits: ${totalhits}`;
+  elements.resultsShowing.innerHTML = `Showing ${start}-${stop} out of ${totalhits} for query: "${query}"`;
+};
 
 export const headerImg = apod => {
-    if(apod.media_type == "video") {
-        const style = `linear-gradient(180deg, #15141200 0%, #2B2F2D33 29%, #101110 100%) center/cover`;
-        elements.header.style.background = style;
-        const iframe = `<iframe src="${apod.url}?vq=hd1080&autoplay=1&loop=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1" width="100%" height="100%" frameborder="0"></iframe>`;
-        elements.header.innerHTML = iframe;
-    }else {
-        const style = `linear-gradient(180deg, #15141200 0%, #2B2F2D33 29%, #101110 100%), url(${apod.url}) center/cover`;
-        elements.header.style.background = style;
-    }
-    elements.headerTitle.innerHTML = apod.title;
-    elements.headerDetails.innerHTML = `<q>${apod.explanation}</q>`;
+  if (apod.media_type == "video") {
+    const style = `linear-gradient(180deg, #15141200 0%, #2B2F2D33 29%, #101110 100%) center/cover`;
+    elements.header.style.background = style;
+    const iframe = `<iframe src="${apod.url}?vq=hd1080&autoplay=1&loop=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1" width="100%" height="100%" frameborder="0"></iframe>`;
+    elements.header.innerHTML = iframe;
+  } else {
+    const style = `linear-gradient(180deg, #15141200 0%, #2B2F2D33 29%, #101110 100%), url(${apod.url}) center/cover`;
+    elements.header.style.background = style;
+  }
+  elements.headerTitle.innerHTML = apod.title;
+  elements.headerDetails.innerHTML = `<q>${apod.explanation}</q>`;
 };
 
 export const headerPopup = apod => {
-    if(apod.media_type == "video") {
-        elements.header__popupImg.innerHTML = `<iframe src="${apod.url}?vq=hd1080&autoplay=1&loop=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1" width="100%" height="100%" frameborder="0"></iframe>`; 
-        elements.header__popup.style.display = 'flex'; 
-    }else {
-        elements.header__popupImg.style.background = `url(${apod.url}) center/contain no-repeat`; 
-        elements.header__popup.style.display = 'flex'; 
-    }
+  if (apod.media_type == "video") {
+    elements.header__popupImg.innerHTML = `<iframe src="${apod.url}?vq=hd1080&autoplay=1&loop=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1" width="100%" height="100%" frameborder="0"></iframe>`;
+    elements.header__popup.style.display = "flex";
+  } else {
+    elements.header__popupImg.style.background = `url(${apod.url}) center/contain no-repeat`;
+    elements.header__popup.style.display = "flex";
+  }
 };
 
 export const clearInput = () => {
-    elements.searchInput.value = ``;
+  elements.searchInput.value = ``;
 };
 
 export const clearResults = () => {
-    elements.searchResItems.innerHTML = ``;
-}
+  elements.searchResItems.innerHTML = ``;
+};
 
 //clickin on an image to popup carousel
 let search;
-export const giveState = (state) => {
-    search = state.data.items;
-}
-const getFontSize = (textLength) => {
-    const baseSize = 13;
-    let fontSize = 4.5;
-    while(textLength >= baseSize) {
-      fontSize -= .2;
-      textLength -= 4;
-    }
-    return `${fontSize}vw`;
+export const giveState = state => {
+  search = state.data.items;
+};
+const getFontSize = textLength => {
+  const baseSize = 13;
+  let fontSize = 4.5;
+  while (textLength >= baseSize) {
+    fontSize -= 0.2;
+    textLength -= 4;
   }
+  return `${fontSize}vw`;
+};
 
 const renderData = (search, id) => {
-    for (let x = 0; x < search.length; x++) {
-        if(id == search[x].data[0].nasa_id) {
-            if(x == 0) {
-                elements.img_left.style.display = 'none';
-                elements.img_right.style.display = 'block';
-            }else if(x == search.length - 1) {
-                elements.img_left.style.display = 'block';
-                elements.img_right.style.display = 'none';
-            }else {
-                elements.img_right.style.display = 'block';
-                elements.img_left.style.display = 'block';
-            }
-            const data = {
-                description: search[x].data[0].description,
-                title: search[x].data[0].title,
-                date: search[x].data[0].date_created,
-                creator: search[x].data[0].secondary_creator,
-                center: search[x].data[0].center,
-                keyword: search[x].data[0].keywords[0],
-            }
-            elements.img__title.innerHTML = data.title;
-            elements.img__title.style.fontSize = getFontSize(data.title.length);
-            elements.img__details.innerHTML = data.description;
-            elements.img__creator.innerHTML = `<b>date_created</b>: ${data.date} </br><b>secondary_creator</b>: ${data.creator}</br><b>center</b>: ${data.center}`;
-            elements.img_keyword.innerHTML = `<b>Keyword: </b><em>${data.keyword}</em>`;
-        }
-    };    
-}
+  for (let x = 0; x < search.length; x++) {
+    if (id == search[x].data[0].nasa_id) {
+      if (x == 0) {
+        elements.img_left.style.display = "none";
+        elements.img_right.style.display = "block";
+      } else if (x == search.length - 1) {
+        elements.img_left.style.display = "block";
+        elements.img_right.style.display = "none";
+      } else {
+        elements.img_right.style.display = "block";
+        elements.img_left.style.display = "block";
+      }
+      const data = {
+        description: search[x].data[0].description,
+        title: search[x].data[0].title,
+        date: search[x].data[0].date_created,
+        creator: search[x].data[0].secondary_creator,
+        center: search[x].data[0].center,
+        keyword: search[x].data[0].keywords[0]
+      };
+      elements.img__title.innerHTML = data.title;
+      elements.img__title.style.fontSize = getFontSize(data.title.length);
+      elements.img__details.innerHTML = data.description;
+      elements.img__creator.innerHTML = `<b>date_created</b>: ${data.date} </br><b>secondary_creator</b>: ${data.creator}</br><b>center</b>: ${data.center}`;
+      elements.img_keyword.innerHTML = `<b>Keyword: </b><em>${data.keyword}</em>`;
+    }
+  }
+};
 
-elements.searchResItems.addEventListener('click', e => {
-    const targetDiv = e.target.closest('div')
-    const targetImgID = targetDiv.id;
-    const markup = `
+elements.searchResItems.addEventListener("click", e => {
+  const targetDiv = e.target.closest("div");
+  const targetImgID = targetDiv.id;
+  const markup = `
     <div class="img" id="${targetImgID}" style="background: url(https://images-assets.nasa.gov/image/${targetImgID}/${targetImgID}~orig.jpg) center/contain no-repeat"></div>
-    `; 
-    renderData(search, targetImgID);
-    elements.img__container.innerHTML = markup;
-    elements.carousel.style.display = 'grid';
+    `;
+  renderData(search, targetImgID);
+  elements.img__container.innerHTML = markup;
+  elements.carousel.style.display = "grid";
 });
 
-elements.header__popupClose.addEventListener('click', e => {
-    e.preventDefault();
-    elements.carousel.style.display = 'none';
-    elements.header__popup.style.display = 'none';
+elements.header__popupClose.addEventListener("click", e => {
+  e.preventDefault();
+  elements.carousel.style.display = "none";
+  elements.header__popup.style.display = "none";
 });
 
-elements.carouselClose.addEventListener('click', e => {
-    e.preventDefault();
-    elements.carousel.style.display = 'none';
-
+elements.carouselClose.addEventListener("click", e => {
+  e.preventDefault();
+  elements.carousel.style.display = "none";
 });
 
-document.addEventListener('keydown', e => {
-    if(e.keyCode === 27 && elements.carousel.style.display != 'none') {
-        elements.popupClose.click();
+document.addEventListener("keydown", e => {
+  if (e.keyCode === 27 && elements.carousel.style.display != "none") {
+    elements.popupClose.click();
+  }
+});
+
+elements.img_right.addEventListener("click", () => {
+  const currentID = elements.img_right.previousElementSibling.children[0].id;
+  let nextID = 0;
+  for (let x = 0; x < search.length; x++) {
+    if (currentID == search[x].data[0].nasa_id) {
+      nextID = search[x + 1].data[0].nasa_id;
     }
-});
-
-elements.img_right.addEventListener('click', () => {
-    const currentID = elements.img_right.previousElementSibling.children[0].id;
-    let nextID = 0;
-    for (let x = 0; x < search.length; x++) {
-        if (currentID == search[x].data[0].nasa_id) {
-            nextID = search[x+1].data[0].nasa_id;
-        }
-    }
-    const markup = `
+  }
+  const markup = `
     <div class="img" id="${nextID}" style="background: url(https://images-assets.nasa.gov/image/${nextID}/${nextID}~orig.jpg) center/contain no-repeat"></div>
     `;
-    elements.img__container.innerHTML = markup;
-    renderData(search, nextID);
+  elements.img__container.innerHTML = markup;
+  renderData(search, nextID);
 });
 
-document.addEventListener('keydown', e => {
-    if(e.keyCode === 39 && elements.img_right.style.display != 'none') {
-        elements.img_right.click();
-    }
+document.addEventListener("keydown", e => {
+  if (e.keyCode === 39 && elements.img_right.style.display != "none") {
+    elements.img_right.click();
+  }
 });
 
-elements.img_left.addEventListener('click', () => {
-    const currentID = elements.img_right.previousElementSibling.children[0].id;
-    let prevID = 0;
-    for (let x = 0; x < search.length; x++) {
-        if (currentID == search[x].data[0].nasa_id) {
-            prevID = search[x-1].data[0].nasa_id;
-        }
+elements.img_left.addEventListener("click", () => {
+  const currentID = elements.img_right.previousElementSibling.children[0].id;
+  let prevID = 0;
+  for (let x = 0; x < search.length; x++) {
+    if (currentID == search[x].data[0].nasa_id) {
+      prevID = search[x - 1].data[0].nasa_id;
     }
-    const markup = `
+  }
+  const markup = `
     <div class="img" id="${prevID}" style="background: url(https://images-assets.nasa.gov/image/${prevID}/${prevID}~orig.jpg) center/contain no-repeat"></div>
     `;
-    elements.img__container.innerHTML = markup;
-    renderData(search, prevID);
+  elements.img__container.innerHTML = markup;
+  renderData(search, prevID);
 });
 
-document.addEventListener('keydown', e => {
-    if(e.keyCode === 37 && elements.img_left.style.display != 'none') {
-        elements.img_left.click();
-    }
+document.addEventListener("keydown", e => {
+  if (e.keyCode === 37 && elements.img_left.style.display != "none") {
+    elements.img_left.click();
+  }
 });
